33ad5260ad53716e3fd8076ddd433bc293664652
Adrian Meinken <meinken.adrian@proton.me>
2022-09-28 11:29:24 +0200

    styling

--- django_project/accounts/models.py
+++ django_project/accounts/models.py
@@ -3,6 +3,3 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
   age = models.PositiveIntegerField(null=True, blank=True)
-
-  def __str__(self):
-    return self.username
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     # APPS
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    "pages.apps.PagesConfig",
 ]
 
 
@@ -130,3 +135,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home" 
 LOGOUT_REDIRECT_URL = "home"
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
